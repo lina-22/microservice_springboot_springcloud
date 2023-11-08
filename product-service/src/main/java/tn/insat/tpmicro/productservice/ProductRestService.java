7f5a097276937cca5b38e090ee62e9c45c61be83
lina-22 <haquefahinur@gmail.com>
2023-11-08 16:06:13 +0100

    added discovery-service and proxy-service

--- product-service/src/main/java/tn/insat/tpmicro/productservice/ProductRestService.java
+++ product-service/src/main/java/tn/insat/tpmicro/productservice/ProductRestService.java
@@ -1,7 +1,10 @@
 //package tn.insat.tpmicro.productservice;
+//import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
+//
 //@RestController
 //public class ProductRestService {
 //    @Value("${me}")
--- proxy-service/src/main/java/tn/insat/tpmicro/proxyservice/ProxyServiceApplication.java
+++ proxy-service/src/main/java/tn/insat/tpmicro/proxyservice/ProxyServiceApplication.java
@@ -1,15 +1,16 @@
-package tn.insat.tpmicro.productservice;
+package tn.insat.tpmicro.proxyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ProductServiceApplication {
+public class ProxyServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(ProxyServiceApplication.class, args);
 	}
 
 }
