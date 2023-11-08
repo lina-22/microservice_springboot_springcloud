f236b3b1de026d6666634c4ce98eca198784ccb1
lina-22 <haquefahinur@gmail.com>
2023-11-08 23:54:37 +0100

    exercise completed | all problem resolved

--- product-service/src/main/java/tn/insat/tpmicro/productservice/ProductRestService.java
+++ product-service/src/main/java/tn/insat/tpmicro/productservice/ProductRestService.java
@@ -1,17 +1,15 @@
-//package tn.insat.tpmicro.productservice;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class ProductRestService {
-//    @Value("${me}")
-//    private String me;
-//    @RequestMapping("/messages")
-//    public String tellMe(){
-//        System.out.println("c'est moi qui ai répondu!");
-//        return me;
-//    }
-//}
+package tn.insat.tpmicro.productservice;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductRestService {
+    @Value("${me}")
+    private String me;
+    @RequestMapping("/messages")
+    public String tellMe(){
+        System.out.println("c'est moi qui ai répondu!");
+        return me;
+    }
+}
